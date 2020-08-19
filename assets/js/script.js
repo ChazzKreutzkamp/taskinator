81a047a9e8ae251e487febbd5034e3fe3ee9cc09
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-08-18 18:17:02 -0600

    user input is added to list items.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,15 @@ var createTaskHandler = function() {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoE1);
     tasksToDoEl.appendChild(listItemEl);
 }
-
 formE1.addEventListener("submit", createTaskHandler);
